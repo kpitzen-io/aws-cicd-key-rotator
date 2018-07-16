28a6a10a230104eda173d41942f03f1b87bfbbe1
Kyle Pitzen <kyle.pitzen@gmail.com>
2018-07-16 18:04:08 -0500

    Testing this

--- terraform.tf
+++ terraform.tf
@@ -39,11 +39,11 @@ EOF
 }
 
 resource "aws_lambda_function" "test_lambda" {
-  filename         = "key_rotator.zip"
+  filename         = "build/key_rotator.zip"
   function_name    = "keyRotator"
   role             = "${aws_iam_role.iam_for_lambda.arn}"
   handler          = "exports.handler"
-  source_code_hash = "${base64sha256(file("key_rotator.zip"))}"
+  source_code_hash = "${base64sha256(file("build/key_rotator.zip"))}"
   runtime          = "nodejs8.10"
 
   environment {
