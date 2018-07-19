32f86f8de8f0cd03a98bb490ca430bd7a44edc4e
Kyle Pitzen <kyle.pitzen@gmail.com>
2018-07-18 21:31:25 -0500

    Add small logging statement indicating success

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const keyRotator = require('src/gitlab-aws-key-rotator');
 const handler = (event) => {
     const apiKey = event.apiKey;
     const groupId = event.groupId;
-    const awsUser = event.awsUser;    
+    const awsUser = event.awsUser;
+    console.log('Keys successfully set!')
     return keyRotator.rotateKeys(groupId, apiKey, awsUser);
 };
 
