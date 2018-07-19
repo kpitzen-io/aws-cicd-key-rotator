0b0c11de71baed9ae99617819e0391ffac52cb9f
Kyle Pitzen <kyle.pitzen@gmail.com>
2018-07-18 20:56:31 -0500

    feat: Moves lambda environment variables into function call args

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const keyRotator = require('src/gitlab-aws-key-rotator');
 
-const apiKey = process.env['API_KEY'];
-const groupId = process.env['GROUP_ID'];
-const awsUser = process.env['AWS_USER'];
-
-const handler = () => {
+const handler = (event) => {
+    const apiKey = event.apiKey;
+    const groupId = event.groupId;
+    const awsUser = event.awsUser;    
     return keyRotator.rotateKeys(groupId, apiKey, awsUser);
 };
 
